40ca820f2ae3d7d13c32d7aa3665175611917901
Holly Henske <hcover333@gmail.com>
2025-09-07 17:07:19 -0500

    All sections pretty close, mobile only

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer>
-      <h2>Footer</h2>
+    <footer className="footer">
+      <img className="logo2" src="./public/images/logo-light.svg" />
+      <div className="social-row">
+        <img src="./public/images/icon-facebook.svg" />
+        <img src="./public/images/icon-youtube.svg" />
+        <img src="./public/images/icon-twitter.svg" />
+        <img src="./public/images/icon-pinterest.svg" />
+        <img src="./public/images/icon-instagram.svg" />
+      </div>
+      <ul className="link-column">
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Careers</li>
+        <li>Support</li>
+        <li>Privacy Policy</li>
+      </ul>
+      <button className="cta-button">Request Invite</button>   
     </footer>
   );
 };

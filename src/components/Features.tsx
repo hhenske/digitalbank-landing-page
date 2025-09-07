b882f48a905dde876df0b7694f151a7188adef29
Holly Henske <hcover333@gmail.com>
2025-09-07 07:58:44 -0500

    started on the Features section

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './Features.css';
 
 const Features = () => {
   return (
-    <section>
-      <h2>Features Section</h2>
+    <section className="feature-header">
+      <h1>Next generation digital banking</h1>
+      <p>Take your financial life online. Your Digitalbank account will be a one-stop shop for spending, saving, budgeting, investing, and much more.</p>
     </section>
   );
 };

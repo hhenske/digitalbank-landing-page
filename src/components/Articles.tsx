44963e662eb93f5b7277edde45d18b04b3d538b0
Holly Henske <hcover333@gmail.com>
2025-09-07 16:36:25 -0500

    Feature and article sections done

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "./Articles.css";
 
 const Articles = () => {
   return (
-    <section>
+    <section className="articles-container">
       <h2>Latest Articles</h2>
+      
+      <img src="./public/images/image-currency.jpg" />
+      <div className="article">
+        <p className="small-body">by Claire Robinson</p>
+        <h4>Receive money in any currency with no fees</h4>
+        <p className="reg-body">The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single...</p>
+      </div>
+      <img src="./public/images/image-restaurant.jpg" />
+      <div className="article">
+        <p className="small-body">by Wilson Hutton</p>
+        <h4>Treat yourself without worrying about money</h4>
+        <p className="reg-body">Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you...</p>
+      </div>
+      <img src="./public/images/image-plane.jpg" />
+      <div className="article">
+        <p className="small-body">by Wilson Hutton</p>
+        <h4>Take your Digitalcard wherever you go</h4>
+        <p className="reg-body">We want you to enjoy your travels. This is why we don't change any fees on purchases while you're abroad. We'll even...</p>
+      </div>
+      <img src="./public/images/image-confetti.jpg" />
+      <div className="article">
+        <p className="small-body">by Claire Robinson</p>
+        <h4>Our invite-only Beta accounts are now live!</h4>
+        <p className="reg-body">After a lot of hard work by our whole team, we are excited to launch our closed beta. It's easy to request an invite through...</p>
+      </div>
     </section>
   );
 };
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,10 +3,29 @@ import './Features.css';
 
 const Features = () => {
   return (
+    <>
     <section className="feature-header">
-      <h1>Next generation digital banking</h1>
-      <p>Take your financial life online. Your Digitalbank account will be a one-stop shop for spending, saving, budgeting, investing, and much more.</p>
+        <h1>Next generation digital banking</h1>
+        <p>Take your financial life online. Your Digitalbank account will be a one-stop shop for spending, saving, budgeting, investing, and much more.</p>
+        <button className="cta-button">Request Invite</button>
     </section>
+    <div className="features-container">
+        <h2>Why choose Digitalbank?</h2>
+        <p>We leverage Open Banking to turn your bank account into your financial hub. Control your finances like never before.</p>
+        <img className="feature-icon" src="./public/images/icon-online.svg" />
+        <h3>Online Banking</h3>
+        <p>Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.</p>
+        <img className="feature-icon" src="./public/images/icon-budgeting.svg" />
+        <h3>Simple Budgeting</h3>
+        <p>See exactly where your money goes each month. See notifications when you're close to your limits.</p>
+        <img className="feature-icon" src="./public/images/icon-onboarding.svg" />
+        <h3>Fast Onboarding</h3>
+        <p>We don't do branches. Open your account in minutes online and start taking control of your finances right away.</p>
+        <img className="feature-icon" src="./public/images/icon-api.svg" />
+        <h3>Open API</h3>
+        <p>Manage your savings, investment, pension, and much more from one account. Tracking your money has never been easier.</p>
+    </div>
+    </>
   );
 };
 

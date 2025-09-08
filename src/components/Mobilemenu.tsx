6b08821ddb9ec106cf52c5e24f60369f0986bc4d
Holly Henske <hcover333@gmail.com>
2025-09-08 08:21:45 -0500

    nav menu created, working on functionality

---
+++ src/components/Mobilemenu.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Mobilemenu.css'
+
+const Mobilemenu = () => {
+  return (
+    <div className="mobile-menu">
+        <ul className="link-column">
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>Blog</li>
+        <li>Careers</li>
+        <li>Support</li>
+        <li>Privacy Policy</li>
+      </ul> 
+    </div>
+  );
+};
+
+export default Mobilemenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React { useState } from "react";
 import './Navbar.css'
+import Mobilemenu from "./Mobilemenu";
+
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
-    <nav>
+    <nav className="navbar">
       <img className="logo" src="./public/images/logo-dark.svg"></img>
-      <img className="hamburger" src="./public/images/icon-hamburger.svg"></img>
+      <img className="menu-icon" 
+      src={
+        menuOpen ?
+      "./public/images/icon_close.svg"
+      : "./public/images/icon-hamburger.svg"
+      }
+      alt="Menu toggle"
+      onClick={toggleMenu}
+      />
     </nav>
+
+    
   );
 };
 
